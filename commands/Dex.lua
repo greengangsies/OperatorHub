11d65658997ee71e734a530e4ae6ffa697ffa453
greengangsies <Yockisaaa@outlook.com>
2026-01-03 21:54:38 -0500

    Update Dex.lua

--- commands/Dex.lua
+++ commands/Dex.lua
@@ -1,1 +1,4 @@
 loadstring(game:HttpGet("https://github.com/AZYsGithub/DexPlusPlus/releases/latest/download/out.lua"))()
+
+_G.OperatorHubAddMessage("Shithub Loaded!", _G.OperatorHubColors.green)
+_G.OperatorHubAddMessage("Shitbot Made by Spooky + Sanchez", _G.OperatorHubColors.gold)
